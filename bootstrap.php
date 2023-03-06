f766488421de6869acf8baaade8e0f3602cdccb6
hashen47 <89251246+hashen47@users.noreply.github.com>
2023-03-06 17:58:14 +0530

    edited bootstrap.php file, bind the Validate class to the main App class

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,19 @@
 use Core\App;
 use Core\Database; 
 use Core\Container;
+use Core\Validate;
 
 
 $container = new Container();
-$container->bind("Core\Database", function () {
+
+
+$container->bind(Database::class, function () {
     return new Database();
+});
+
+
+$container->bind(Validate::class, function () {
+    return new Validate();
 });
 
 
