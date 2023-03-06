0281187e387be4966dcfb1d81b334c259e825081
hashen47 <89251246+hashen47@users.noreply.github.com>
2023-03-06 19:01:35 +0530

    edited routes.php, added new routes to the routes.php

--- routes.php
+++ routes.php
@@ -2,3 +2,30 @@
 
 
 $router->get("/", "controllers/index.ctr.php");
+$router->get("/home", "controllers/index.ctr.php");
+$router->get("/posts", "controllers/posts.ctr.php", $logRequired=true);
+
+
+$router->get("/update", "controllers/update.ctr.php", $logRequired=false);
+$router->post("/update", "controllers/update.ctr.php", $logRequired=false);
+
+
+$router->get("/create", "controllers/create.ctr.php", $logRequired=true);
+$router->post("/create", "controllers/create.ctr.php", $logRequired=true);
+
+
+$router->get("/login", "controllers/login.ctr.php");
+$router->post("/login", "controllers/login.ctr.php");
+
+
+$router->get("/register", "controllers/register.ctr.php");
+$router->post("/register", "controllers/register.ctr.php");
+
+
+$router->get("/logout", "controllers/logout.ctr.php");
+
+
+$router->delete("/delete", "controllers/delete.ctr.php");
+
+
+$router->post("/comment", "controllers/comment.ctr.php");
